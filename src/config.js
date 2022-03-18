57d8817956fa45852c9515aa20f9609f0e9115bf
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-03-18 13:51:32 -0400

    updated url to api

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const baseUrl = `${process.env.REACT_APP_API_URL || "https://myteamsprojectmanager-tf6l4.ondigitalocean.app"}`;
+export const baseUrl = `${process.env.REACT_APP_API_URL || "https://myteamsproject-sptc6.ondigitalocean.app"}`;
